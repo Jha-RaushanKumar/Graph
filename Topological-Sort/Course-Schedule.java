310c9be969785ea550fa118eece9fd96ef36b50f
Jha-RaushanKumar <jha.rau@northeastern.edu>
2023-05-31 11:39:27 -0400

    Create Course-Schedule.java

---
+++ Topological-Sort/Course-Schedule.java
@@ -0,0 +1,78 @@
+/*
+207. Course Schedule
+https://leetcode.com/problems/course-schedule/
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+Example 1:
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+ 
+Constraints:
+1 <= numCourses <= 2000
+0 <= prerequisites.length <= 5000
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+All the pairs prerequisites[i] are unique.
+*/
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        //Method: BFS(Topological)
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0; i < numCourses; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0; i < prerequisites.length; i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        int indegree[] = new int[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+
+        int count = 0;
+        while(!q.isEmpty()){
+            int node = q.remove();
+            count++;
+
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.add(it);
+                }
+            }
+        }
+
+        if(count == numCourses){
+            return true;
+        }
+        return false;
+
+    }
+}
+
+/*
+TC: O(N) + O(2E) ~ O(V+E) [N is total nodes(V), 2E is for total degrees as we traverse all adjacent nodes]
+SC: O(N) [For Queue and indegree array]
+*/
